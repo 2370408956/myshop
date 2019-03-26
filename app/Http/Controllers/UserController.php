e070488adbffe0bbcd2cfa4ec24b1f9cc64f06c9
2370408956 <2370408956@qq.com>
2019-03-26 09:35:53 +0800

    add demo

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -24,7 +24,7 @@ class UserController extends Controller
     //购买记录
     public function buyrecord()
     {
-        $goodsinfo=Cart::where('status',2)
+        $goodsinfo=Cart::where(['status'=>2,'u_id'=>session('u_id')])
             ->orderBy('created_at','desc')
             ->join('shop_goods','shop_goods.goods_id','=','shop_cart.goods_id')
             ->get(['goods_name','buy_number','self_price','shop_cart.goods_id','goods_img']);
